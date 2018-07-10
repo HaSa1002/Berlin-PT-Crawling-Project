c3a882c327ca381e185edddc03e05d073fb6084d
HaSa1002 <johawitt@outlook.de>
2018-07-10 02:31:01 +0200

    changed setup

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
 from setuptools import setup
 
-setup(install_requires=[
-          'beautifulsoup4',
-          'lxml'
-      ])
+setup(name='pt-crawler',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/HaSa1002/Berlin-PT-Crawling-Project',
+      author='Johannes Witt',
+      author_email='meinespamanjoha@web.de',
+      license='Unlicense',
+      packages=[''],
+      install_requires=[
+          'lxml',
+          'beautifulsoup4'
+      ],
+      zip_safe=False)
 
